abc3a4e0e1c520cadd49610dfe0bf7be228a9727
Martijn Schippers <m.b.schippers@student.twente.nl>
2024-02-25 14:10:43 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+// server/server.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+const randomColor = require('randomcolor');
+const Board = require('./board.js')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+// create a board
+const board = new Board(30);
+
+io.on('connection', (socket) => {
+  const color = randomColor();
+  console.log('A user connected');
+  io.emit('welcome message', 'a new user joined');
+  socket.emit('board', board.getBoard());
+
+  // Listen for chat messages from clients
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  // Receive a canvas draw, and emit to everyone
+  socket.on('fillCanvas', (data) => {
+    board.makeTurn(data.x, data.y, color);
+    io.emit('fillCanvas', {data, color});
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
